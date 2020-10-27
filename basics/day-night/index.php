456cd364fed8244b32ecd6a3cfeb864ac6e90515
Jobith M Basheer <jobith@three38inc.com>
2020-10-27 18:48:21 +0530

    completed project-1

--- basics/day-night/index.php
+++ basics/day-night/index.php
@@ -1,5 +1,72 @@
-<?php
+<?php 
+  date_default_timezone_set('Asia/Kolkata');
+  // if(time() >= strtotime("18:00:00")) {
+  //   $status = "night";
+  // }
+  // else{
+  //   $status = "day";
+  // }
+  $status = (time() >= strtotime("19:00:00")) ? "night" :  "day" ;
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day/Night Theme</title>
+    <style>
 
-if (time() >= strtotime("14:08:10")) {
-  echo "ok";
-}
+      html, body{
+        margin: 0;
+        padding: 0;
+      }
+
+      body{
+        min-height: 100vh; 
+      }
+
+      h1, h2{
+        margin: 0;
+      }
+      .bg{
+        min-height: 100vh; 
+        background-position: bottom center;
+        background-size: cover;
+        background-repeat: no-repeat;
+      }
+
+      .bg.night{
+        background-image: url('images/night.png');
+      }
+
+      .bg.day{
+        background-image: url('images/day.png');
+      }
+
+      .title{
+        text-align: center;
+        padding-top: 200px;
+      }
+      .date{
+        text-align: center;
+        padding-top: 20px;
+      }
+
+      .night .title,
+      .night .date{
+        color: white;
+      }
+
+      .day .title,
+      .day .date{
+        color: black;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="bg <?php echo $status; ?>">
+      <h1 class="title"><?php echo ucfirst($status); ?>!</h1>
+      <h2 class="date"><?php echo date("l jS \of F Y h:i:s A"); ?></h2>
+    </div>
+  </body>
+</html>
